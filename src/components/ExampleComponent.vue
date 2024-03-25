2a81502da251a8f07553a61c2189721d2f821204
Alexander Stanevko <dardawill@yandex.ru>
2024-03-25 22:47:51 +0300

    Add titles

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,19 +1,11 @@
 <template>
   <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
+    <h1>Url Shortener</h1>
+    <h4 class="text-center">Coming soon...</h4>
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
 import { Todo, Meta } from './models';
 
 interface Props {
@@ -23,15 +15,8 @@ interface Props {
   active: boolean;
 };
 
-const props = withDefaults(defineProps<Props>(), {
+withDefaults(defineProps<Props>(), {
   todos: () => []
 });
 
-const clickCount = ref(0);
-function increment() {
-  clickCount.value += 1;
-  return clickCount.value;
-}
-
-const todoCount = computed(() => props.todos.length);
 </script>
